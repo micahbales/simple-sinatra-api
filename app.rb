8b1d0973f4ff65eca2350e652d99045db43b91a2
Micah Bales <micahbales@gmail.com>
2019-01-16 10:16:56 -0800

    Return Card Data
    * Return all cards at 'api/v1/cards'
    * Return single cards at 'api/v1/cards/:card_id'
    * Encode all responses as 'application/json'
    * Add hot reloading in development with rerun gem

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 require 'sinatra'
+require 'json'
 configure { set :server, :puma }
 
-get '/' do
-  {:hi => 'dad'}.to_json
+# Encode all responses as 'application/json'
+before do
+  content_type :json
+end
+
+get '/api/v1/cards' do
+  File.read('cards.json')
+end
+
+get '/api/v1/cards/:card_id' do
+  cards = JSON.parse(File.read('cards.json'))
+
+  cards.filter! { |card| card["id"] == params[:card_id] }
+
+  cards.to_json
 end
